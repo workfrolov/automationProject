bda6805a146afc1e9dcd96f222ac1d09739b08d5
workfrolov <work.frolov@gmail.com>
2017-10-11 02:27:59 +0300

    TNG asserts

---
+++ src/main/java/MyCode/TestngTestClass.java
@@ -0,0 +1,11 @@
+package MyCode;
+
+public class TestngTestClass {
+    public static boolean printString(String input) {
+        if (input.contains("A-Z")) {
+            System.out.print(input);
+        }
+        return true;
+    }
+
+}
--- src/test/java/TestSuit/Tests.java
+++ src/test/java/TestSuit/Tests.java
@@ -1,10 +1,49 @@
 package TestSuit;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import MyCode.TestngTestClass;
 
 public class Tests {
     @Test
-    public static void myTest(){
-            System.out.println("The first test");
+    public static void WEB1() {
+        Assert.assertFalse(TestngTestClass.printString("FIVE"), "Bad frfrfr");
     }
+
+    @Test
+    public static void WEB2() {
+        TestngTestClass.printString("it IS a TesT");
+    }
+
+    @Test
+    public static void WEB3() {
+        TestngTestClass.printString("!@XXX#");
+    }
+
+    @Test
+    public static void WEB4() {
+        TestngTestClass.printString("5");
+    }
+
+    @Test
+    public static void WEB5() {
+        TestngTestClass.printString("test");
+    }
+
+    @Test
+    public static void WEB6() {
+       TestngTestClass.printString("5*11=12");
+    }
+
 }
+/*
+Positive:
+    FIVE : True
+    it IS a TesT : True
+    !@XXX# : True
+
+Netative
+    5 : False
+    test : False
+    5*11=12 : False
+ */
