21ca305fd086d4ef7d6997aaf192fc0071defcda
workfrolov <work.frolov@gmail.com>
2017-10-11 01:12:03 +0300

    TestNG Test Add

---
+++ src/test/java/TestSuit/Tests.java
@@ -0,0 +1,10 @@
+package TestSuit;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+    @Test
+    public static void myTest(){
+            System.out.println("The first test");
+    }
+}
